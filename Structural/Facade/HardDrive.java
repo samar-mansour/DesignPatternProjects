f2b399231f05c46dca86dad62b1d84377a6a4a75
Summer Mansour <52625367+samar-mansour@users.noreply.github.com>
2022-08-16 20:21:23 +0300

    uploading files

---
+++ Structural/Facade/HardDrive.java
@@ -0,0 +1,7 @@
+package com.samar;
+
+public class HardDrive {
+    public byte[] read(long Iba, int size){
+        return new byte[]{'f','z'};
+    }
+}
---
+++ Structural/Facade/Main.java
@@ -0,0 +1,17 @@
+package com.samar;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        CPU cpu = new CPU();
+        Memory memory = new Memory();
+        HardDrive hd = new HardDrive();
+	    ComputerFacade computerFacade = new ComputerFacade(cpu, memory, hd);
+
+	    computerFacade.start();
+
+
+
+    }
+}
